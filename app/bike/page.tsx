25437f4dca71133baaed53ae7b9ac79d27ffe37d
NICKLEESON1970 <topleeson@hotmail.com>
2026-04-10 17:56:16 -0400

    resilience demo activo

--- app/bike/page.tsx
+++ app/bike/page.tsx
@@ -10,6 +10,7 @@ export default function BikePage() {
   return (
     <main style={{ padding: "2rem" }}>
       <h1>Selecciona tu Bicicleta</h1>
+      <p>Demo Resilience activo</p>
 
       {/* Gravel Pro */}
       <button
